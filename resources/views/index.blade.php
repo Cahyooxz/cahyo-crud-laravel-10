52de9f6cd07a558ac3dad412f32566dcb8a6456c
Cahyooxz <cahyorasyid011627@gmail.com>
2024-02-23 14:08:48 +0700

    add footer and sidebar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,14 @@
 <title>Dashboard</title>
 @section('content')
 <div class="row">
-    <div class="col">
-        <h5>Dashboard</h5>
+    <div class="col-md-6 col-sm-12 d-flex align-items-center">
+        <img src="img/data.gif" alt="data" class="w-75 mt-3">
+    </div>
+    <div class="col-md-6 col-sm-12 d-flex justify-content-center align-items-center">
+        <div class="mt-5">
+            <h1 class="mb-3">Selamat Datang Di Data Post <span class="text-danger">!</span></h1>
+            <a href="{{ route('posts.index') }}" class="btn btn-primary">Mulai Data Post</a>
+        </div>
     </div>
 </div>
 @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,18 +1,23 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div class="container">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+<nav class="navbar bg-primary navbar-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand fw-bold" href="#">DATA POST</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="{{ route('posts.index') }}">Data Post</a>
-              </li>
-          </ul>
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header bg-primary">
+          <h5 class="offcanvas-title text-light" id="offcanvasNavbarLabel">DATA POST</h5>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body p-0">
+          <ul class="navbar-nav justify-content-end flex-grow-1">
+            <li class="nav-item border border-success p-2 border-opacity-10 ps-2" style="height:60px">
+                <a class="nav-link" aria-current="page" href="/"><i class="bi bi-house-door me-2"></i>Dashboard</a>
+            </li>
+            <li class="nav-item border border-success p-2 border-opacity-10 ps-2" style="height:60px">
+                <a class="nav-link" href="{{ route('posts.index') }}"><i class="bi bi-clipboard-data me-2"></i>Data Post</a>
+            </li>
+        </div>
       </div>
-  </div>
-</nav>
+    </div>
+  </nav>
